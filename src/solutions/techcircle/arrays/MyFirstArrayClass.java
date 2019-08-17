5eaaa56bb90a3af1a4e432ad1a68a35c13f50a33
TechCircle <techcircleschool@gmail.com>
2019-08-17 16:58:06 -0400

    commiting again

--- src/solutions/techcircle/arrays/MyFirstArrayClass.java
+++ src/solutions/techcircle/arrays/MyFirstArrayClass.java
@@ -32,7 +32,14 @@ public class MyFirstArrayClass {
 		 int[][] myNumbers = { {1, 2, 3, 4}, {5, 6, 7} };
 		 
 		 
+		 System.out.println(myNumbers[0][0]);
+		 System.out.println(myNumbers[0][1]);
 		 System.out.println(myNumbers[0][2]);
+		 System.out.println(myNumbers[0][3]);
+		 System.out.println(myNumbers[1][0]);
+		 System.out.println(myNumbers[1][1]);
+		 System.out.println(myNumbers[1][2]);
+		 System.out.println(myNumbers[1][3]);
 		 
 //			 
 		 System.out.println(myNumbers.length);
