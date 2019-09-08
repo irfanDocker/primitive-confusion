1eea2b3145dd0a1df2d074302ca922ce0a76ead3
TechCircle <techcircleschool@gmail.com>
2019-09-08 17:32:28 -0400

    Added code about this keywork

--- src/solutions/techcircle/ObjectAndClasses/Car.java
+++ src/solutions/techcircle/ObjectAndClasses/Car.java
@@ -1,5 +1,45 @@
 package solutions.techcircle.ObjectAndClasses;
 
-public class Car {
+public class Car{
+	
+	private String make;
+	private String model;
+	private int year;
+	
+	public String getMake() {
+		return make;
+	}
+	
+	public Car() {
+		this("Tesla","Model X",2020);
+		System.out.println("Car's default constructor");
+		
+	}
+	
+	public Car(String make, String model, int year) {
 
+		this.make = make;
+		this.model = model;
+		this.year = year;
+	}
+	
+	public void setMake(String make) {
+		this.make = make;
+	}
+	public String getModel() {
+		return model;
+	}
+	public void setModel(String model) {
+		this.model = model;
+	}
+	public int getYear() {
+		return year;
+	}
+	public void setYear(int year) {
+		this.year = year;
+	}
+
+	
+	
+	
 }
---
+++ src/solutions/techcircle/ObjectAndClasses/TestCars.java
@@ -0,0 +1,19 @@
+package solutions.techcircle.ObjectAndClasses;
+
+public class TestCars {
+	public static void main(String[] args) {
+		
+		Car car = new Car();
+		
+
+		
+//		car.setMake("Honda");
+//		car.setModel("Pilot");
+//		car.setYear(2019);
+//		
+		System.out.println(car.getMake());
+		System.out.println(car.getModel());
+		System.out.println(car.getYear());
+//		
+	}
+}
